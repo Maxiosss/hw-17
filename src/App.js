a77b790fb06fd6ce862453165b314005cbd67641
Maxiosss <129985137+Maxiosss@users.noreply.github.com>
2025-07-24 13:20:03 +0300

    updated

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { nanoid } from "nanoid";
 import ContactForm from "./components/ContactForm";
 import ContactList from "./components/ContactList";
 import Filter from "./components/Filter";
 import "./App.css";
 
-const INITIAL_CONTACTS = [
-  { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-  { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-  { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-  { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
-];
 
 function App() {
   const [contacts, setContacts] = useState([]);
